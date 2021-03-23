dd345d8a7f3b54b0175464b3ed352f78b93f25f6
Sena <73450328+10Ne2Cx1To426@users.noreply.github.com>
2021-03-23 19:06:14 +0900

    page404

--- src/router/HomeRoutes.tsx
+++ src/router/HomeRoutes.tsx
@@ -1,9 +1,11 @@
 import { Setting } from "../components/pages/Setting";
 import { Home } from "../components/pages/Home";
 import { UserManagement } from "../components/pages/UserManagement";
+import { Page404 } from "../components/pages/Page404";
 
 export const HomeRoutes = [
   { path: "/", exact: true, children: <Home /> },
   { path: "/user_management", exact: false, children: <UserManagement /> },
-  { path: "/setting", exact: true, children: <Setting /> }
+  { path: "/setting", exact: false, children: <Setting /> },
+  { path: "*", exact: false, children: <Page404 /> }
 ];
